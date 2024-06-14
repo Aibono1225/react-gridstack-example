d77a7c6b816903c309c39a49d750a548aa17967f
ChloeLin <chloelin@ssw.com.au>
2024-06-14 15:08:56 +0800

    navigate

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState({});
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     // e.preventDefault();
     try {
-      const response = await axios.get('http://192.168.37.1:15000/api/auth/windows-auth', { withCredentials: true });
+      const response = await axios.get(
+        "http://192.168.37.1:15000/api/auth/windows-auth",
+        { withCredentials: true }
+      );
       // const response = await axios.get('http://localhost:5000/api/auth/windows-auth', { withCredentials: true });
       // setMessage(response.data.message);
-      return response
+      return response;
     } catch (error) {
       if (error.response) {
         setMessage(error.response.data.message);
       } else {
-        setMessage('An error occurred');
+        setMessage("登录失败");
       }
     }
   };
 
   useEffect(() => {
     const showToken = async () => {
-      const response = await handleLogin()
-      // const token = response.data.token
-      setMessage(response.data)
-    }
+      const response = await handleLogin();
+      if (!response?.data?.user) {
+        setTimeout(() => {
+          navigate("/demo");
+        }, 5000);
+      }
+      setMessage(response.data);
+    };
 
-    showToken()
-  }, [])
+    showToken();
+  }, []);
 
   return (
     <div>
@@ -59,10 +68,10 @@ const Login = () => {
       {/* <button type="submit" onClick={handleLogin}>Login</button> */}
       {message.token && (
         <>
-        <p>登录成功！</p>
-        <p>您的用户名：{message.user.displayName}</p>
-        <p>所属公司：{message.user.company}</p>
-        <p>所在部门：{message.user.department}</p>
+          <p>登录成功！</p>
+          <p>您的用户名：{message.user.displayName}</p>
+          <p>所属公司：{message.user.company}</p>
+          <p>所在部门：{message.user.department}</p>
         </>
       )}
     </div>
